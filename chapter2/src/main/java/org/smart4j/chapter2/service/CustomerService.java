076eef05783e9131a0836c4a0a0c323fd17ae730
AlanYume <875785679@qq.com>
2016-01-10 15:00:17 +0800

    extract sql connection and close method to databaseHelper class

--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,6 +1,8 @@
 package org.smart4j.chapter2.service;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
 import org.smart4j.chapter2.util.PropsUtil;
 
@@ -12,33 +14,14 @@ import java.util.Properties;
 
 public class CustomerService {
 
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
-
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
-
-    static {
-        Properties conf = PropsUtil.loadProps("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
-
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("can not load jdbc driver", e);
-        }
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     public List<Customer> getCustomerList() {
         Connection conn = null;
         try {
             List<Customer> customerList = new ArrayList<>();
             String sql = "SELECT * FROM customer";
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            conn = DatabaseHelper.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
@@ -55,13 +38,7 @@ public class CustomerService {
         } catch (SQLException e) {
             LOGGER.error("execute sql failure", e);
         } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    LOGGER.error("close connection failure", e);
-                }
-            }
+            DatabaseHelper.closeConnection(conn);
         }
         return null;
     }
